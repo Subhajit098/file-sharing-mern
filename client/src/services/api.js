4fb8e78e8fc6d206f2055945704e9fc5040392f4
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2023-06-12 12:38:53 +0530

    Add the render api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const URL = "http://localhost:5000";
+// const URL = "http://localhost:5000";
+const URL = "https://file-sharing-mern-gzab.onrender.com";
 
 export const uploadFile= async(data)=>{
     try{
